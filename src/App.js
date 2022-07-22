c2b04242f20bba3c6cc933e983dc6e53af794ff9
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-07-22 12:05:30 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@ import "./styles.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
-  const [data, setData] = useState(null);
-
-  console.log({ data });
+  const [page, setPage] = useState(1)
+  const [data, setData] = useState([]);
 
   // Write code here.
   //
 
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/${dataType}/?page=${page}`)
+    .then(resp => resp.json())
+    .then(resp => {
+      console.log('my results', resp)
+      setData(resp.results)
+    })
+  }, [dataType, page])
+
   return (
     <div>
-      <SelectTypeForm setDataType={setDataType} />
-      {data && <DataList dataType={dataType} data={data.results} />}
+      <SelectTypeForm setDataType={setDataType}/>
+      {data && <DataList dataType={dataType} data={data} />}
+      { (!data.length) && (
+      <button
+        onClick={() => setPage(page+1)}>Next Page</button>)}
+        { (page > 1) && (
+      <button
+        onClick={() => setPage(page-1)}
+        >Previous</button>
+        )}
     </div>
   );
 }
